2bdd1c42dfe5826206d50208c10eda16916a985a
Danny Gershman <danny.gershman@gmail.com>
2018-10-09 23:53:28 -0400

    region lookup

--- scripts/tomato.js
+++ scripts/tomato.js
@@ -9,6 +9,7 @@
 //
 // Commands:
 //   hubot tomato last import - returns the time of the last sync
+//   hubot what region is <area> - returns the region a given area is in
 //
 // Notes:
 // 
@@ -18,15 +19,13 @@
 var moment = require('moment');
 
 module.exports = robot => {
+  var tomatoBaseUrl = 'https://tomato.na-bmlt.org';
 
   robot.respond(/tomato last import/, msg => {
     requestGet(robot,
-      'https://tomato.na-bmlt.org/rest/v1/rootservers/?format=json',
-      {
-        'User-Agent' : '+fortyseven'
-      },
+      `${tomatoBaseUrl}/rest/v1/rootservers/?format=json`,
       res => {
-        var maxTime;
+        let maxTime = null;
         for (item of JSON.parse(res)) {
           if (maxTime == null || Date.parse(item['last_successful_import']) < maxTime) {
             maxTime = Date.parse(item['last_successful_import'])
@@ -37,10 +36,30 @@ module.exports = robot => {
       });
   });
 
-  var requestGet = (msg, url, headers, cb) => {
+  robot.respond(/what region has (.*)/, msg => {
+    requestGet(robot,
+      `${tomatoBaseUrl}/main_server/client_interface/json/?switcher=GetServiceBodies`,
+      res => {
+        for (item of JSON.parse(res)) {
+          if (item['type'] === 'AS'
+            && item['name'].trim().toLowerCase() === msg.match[1].trim().toLowerCase()) {
+            for (let parent of JSON.parse(res)) {
+              if (parent['id'] === item['parent_id']) {
+                msg.send(`🍅 says the region is ${parent['name']}.`);
+              }
+            }
+          }
+        }
+      }
+    )
+  });
+
+  var requestGet = (msg, url, cb) => {
     msg
       .http(url)
-      .headers(headers)
+      .headers({
+        'User-Agent' : '+fortyseven'
+      })
       .get()((err, res, body) => {
         cb(body);
       });
