73e9cdfa34daa07b9d7149e7dbbc17327a86c1d0
Danny Gershman <danny.gershman@gmail.com>
2018-10-12 15:58:27 -0400

    Update hubot.cmd

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -4,4 +4,4 @@ call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-node_modules\.bin\hubot.cmd --name "fortyseven" %* 
+node_modules\.bin\hubot.cmd --name "47" %* 
