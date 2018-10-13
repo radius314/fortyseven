e1c6db7c972a8aa551800c39fc40f99a38e48d29
Danny Gershman <danny.gershman@gmail.com>
2018-10-13 16:31:11 -0400

    how to get people involved #1

---
+++ scripts/involved.js
@@ -0,0 +1,62 @@
+// Description:
+//   Tools for the community involvement
+//
+// Dependencies:
+//
+//
+// Configuration:
+//   HUBOT_GITHUB_TOKEN
+//
+// Commands:
+//   hubot how can i help - returns a list of "help wanted" issues in select repos
+//
+// Notes:
+//
+//
+// Author:
+//   radius314
+let utils = require('./utils.js');
+let reposList = [
+  "radius314/bread",
+  "radius314/crouton",
+  "radius314/lettuce",
+  "radius314/yap",
+  "radius314/yap-fbmessenger-bot",
+  "littlegreenviper/bmlt-root-server",
+  "jbraswell/tomato"
+];
+
+module.exports = robot => {
+  let github = require("githubot")(robot);
+
+  robot.respond(/how can i help/i, msg => {
+    const notFoundResponse = "I couldn't discover something automatically... I'm sure you could find something, if you ask a human.";
+    let response = [];
+
+    Promise.all(reposList.map(repo => {
+      return new Promise((resolve, reject) => {
+        github.get(`repos/${repo}/issues`, (issues) => {
+          for (issue of issues) {
+            if (hasLabels(issue['labels'], "help wanted")) {
+              response.push(`${issue['title']}: ${issue['html_url']}`);
+            }
+          }
+
+          resolve();
+        });
+      })
+    })).then(() => {
+      msg.send(response.length > 0 ? response.join("\n") : notFoundResponse);
+    });
+  })
+};
+
+function hasLabels(labels, name) {
+  for (let label of labels) {
+    if (label['name'] === name) {
+      return true;
+    }
+  }
+
+  return false;
+}
