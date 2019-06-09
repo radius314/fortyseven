b6cf133ee8ec290c4c331ec4d25d5da7a533e997
Danny Gershman <danny.gershman@gmail.com>
2019-06-09 19:16:25 -0400

    enumerate all repos versus using a pre-set list #10

--- scripts/involved.js
+++ scripts/involved.js
@@ -15,21 +15,6 @@
 //
 // Author:
 //   radius314
-let utils = require('./utils.js');
-let reposList = [
-  "bmlt-enabled/tomato",
-  "bmlt-enabled/bmlt-root-server",
-  "bmlt-enabled/bmlt-wordpress-satellite-plugin",
-  "bmlt-enabled/bmlt-portal",
-  "bmlt-enabled/list-locations-bmlt",
-  "bmlt-enabled/bread",
-  "bmlt-enabled/crouton",
-  "bmlt-enabled/fortyseven",
-  "bmlt-enabled/lettuce",
-  "bmlt-enabled/sdle",
-  "bmlt-enabled/yap",
-  "bmlt-enabled/yap-bots",
-];
 
 module.exports = robot => {
   let github = require("githubot")(robot);
@@ -38,22 +23,24 @@ module.exports = robot => {
     const notFoundResponse = "I couldn't discover something automatically... I'm sure you could find something, if you ask a human.";
     let response = [];
 
-    Promise.all(reposList.map(repo => {
-      return new Promise((resolve, reject) => {
-        github.get(`repos/${repo}/issues`, (issues) => {
-          for (issue of issues) {
-            if (hasLabels(issue['labels'], "help wanted") || hasLabels(issue['labels'], "needs qa")) {
-              if (issue['assignee'] == null) {
-                response.push(`${issue['title']}: ${issue['html_url']} _${issue['labels'].map(value => value['name']).join(",")}_`);
+    github.get('orgs/bmlt-enabled/repos', (reposList) => {
+      Promise.all(reposList.map(repo => {
+        return new Promise((resolve, reject) => {
+          github.get(`repos/${repo.full_name}/issues`, (issues) => {
+            for (issue of issues) {
+              if (hasLabels(issue['labels'], "help wanted") || hasLabels(issue['labels'], "needs qa")) {
+                if (issue['assignee'] == null) {
+                  response.push(`${issue['title']}: ${issue['html_url']} _${issue['labels'].map(value => value['name']).join(",")}_`);
+                }
               }
             }
-          }
 
-          resolve();
-        });
-      })
-    })).then(() => {
-      msg.send(response.length > 0 ? response.join("\n") : notFoundResponse);
+            resolve();
+          });
+        })
+      })).then(() => {
+        msg.send(response.length > 0 ? response.join("\n") : notFoundResponse);
+      });
     });
   });
 };
